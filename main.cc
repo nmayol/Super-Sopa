948ea1fc9ba53ea5a532021b9603cb9733489681
nmayol <02mayolneus@gmail.com>
2022-10-03 16:58:54 +0200

    faltava un ;

--- main.cc
+++ main.cc
@@ -8,7 +8,7 @@ int main () {
     // n es la mida de la sopa
     cout << "introdueix la mida de la sopa: (el vector de prova funciona amb mida >= 7)." << endl;
     int n; cin >> n;
-    cout << "ara es crearà la sopa (pot trigar una mica)."
+    cout << "ara es crearà la sopa (pot trigar una mica)." << endl;
     superSopa s = superSopa(n);
 
     // poso aixo per provar
