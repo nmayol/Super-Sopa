4939f51ee299a17a6cd50031ff7140b519e81faf
jordipalomeraa <jordipalomera21@gmail.com>
2022-10-12 22:11:12 +0200

    filtre de bloom

--- Filtre Bloom/diccBloomFilter.cc
+++ Filtre Bloom/diccBloomFilter.cc
@@ -9,21 +9,13 @@ BloomFilterDictionary::BloomFilterDictionary () {
 
 void BloomFilterDictionary::afegir (const string& s) {
     int a = h1(s);
-    //cout << 'a';
     int b = h2(s);
-    //cout << 'b';
     int c = h3(s);
-    //cout << 'c';
     int d = h4(s);
-    //cout << 'd';
     int e = h5(s);
-    //cout << 'e';
     int f = h6(s);
-    //cout << 'f';
     int g = h7(s);    
 
-    //cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << f << ' ' << g << endl;
-    //cout << "hashos" << endl;
     BloomFilter[a] = true;
     BloomFilter[b] = true;
     BloomFilter[c] = true;
@@ -46,18 +38,14 @@ bool BloomFilterDictionary::comprovar (const string& s) {
         BloomFilter[d] && BloomFilter[e] && BloomFilter[f] && BloomFilter[g];
 }
 
-//polinòmiques
 int BloomFilterDictionary::h1 (const string& s) {
     long long int hash = 0;
 
     for (int i = 0; i < s.size(); ++i) {
-        //cout << s[i] << ' ';
         int valor = (int)s[i];
         hash = 11*hash + valor;
     }
-    //cout << "ooo" << ' ';
-    int res = hash % mida;
-    //cout << res << endl;
+
     return abs(hash % mida);
 }
 
@@ -67,7 +55,6 @@ int BloomFilterDictionary::h2 (const string& s) {
     for (int i = 0; i < s.size(); ++i) {
         int valor = (int)s[i];
         hash = 31*hash + valor;
-        //cout << "o " << hash << endl;
     }
 
     return abs(hash % mida);
@@ -143,45 +130,44 @@ int BloomFilterDictionary::h7 (const string& s) {
 
 
 int main () {
-    BloomFilterDictionary b;
+    vector<string> paths = {"../diccionaris/mare-balena-vocabulary-3.txt",
+                            "../diccionaris/dracula-vocabulary-4.txt",
+                            "../diccionaris/quijote-vocabulary-3.txt"};
 
-    vector<string> totes = {"espasmodicament"};   //paraules que queden per afegir al diccionari
-    
+    for (string path : paths) {
+        cout << "Fitxer: " << path << endl;
 
-    ifstream fp_in;
-    string a;
-    fp_in.open("../mare-balena-vocabulary-3.txt");
-    while (fp_in >> a) {
-        totes.push_back(a);
-    }
-    fp_in.close();  
-    //cout << 'e';
-    int errors = 0, encerts = 0, comprovacions = 0;
-    for (int i = 0; i < totes.size(); ++i) {
-        string w = totes[i];
-        cout << w << ": " << endl;
-        int e = 0, e2 = 0;
-        //cout << 'e';
-        b.afegir(w);
-        //cout << 'e';
-        for (int j = 0; j < i+1; ++j) {
-            ++comprovacions;
-            if (not b.comprovar(totes[j])) ++e;
-            else ++e2;
+        BloomFilterDictionary b;
+        vector<string> totes;  
+
+        ifstream fp_in;
+        string a;
+        fp_in.open(path);
+        while (fp_in >> a) {
+            totes.push_back(a);
+        }
+        fp_in.close(); 
+
+        int errors = 0, encerts = 0, comprovacions = 0;
+        for (int i = 0; i < totes.size(); ++i) {
+            string w = totes[i];
+            b.afegir(w);
+
+            for (int j = 0; j < i+1; ++j) {
+                ++comprovacions;
+                if (not b.comprovar(totes[j])) ++errors;
+                else ++encerts;
+            }
+
+            for (int j = i+1; j < totes.size(); ++j) {
+                ++comprovacions;
+                if (b.comprovar(totes[j])) ++errors;
+                else ++encerts;
+            }
         }
 
-        for (int j = i+1; j < totes.size(); ++j) {
-            ++comprovacions;
-            if (b.comprovar(totes[j])) ++e;
-            else ++e2;
-        }
-
-        cout << "Errors: " << e << " Encerts: " << e2 << endl;
-        errors += e;
-        encerts += e2;
-    }
-
-    cout << "Errors totals: " << errors << endl;
-    cout << "Encerts totals: " << encerts << endl;
-    cout << "Comprovacions: " << comprovacions << endl;
+        cout << "Errors totals: " << errors << endl;
+        cout << "Encerts totals: " << encerts << endl;
+        cout << "Comprovacions: " << comprovacions << endl;
+    }    
 }
