8c0e47ccfd154d6c1ea03391e65654d8a8204f76
jordipalomeraa <jordipalomera21@gmail.com>
2022-10-12 12:58:50 +0200

    Bloom Filter

--- Filtre Bloom/diccBloomFilter.cc
+++ Filtre Bloom/diccBloomFilter.cc
@@ -1,9 +1,149 @@
 #include "diccBloomFilter.hh"
 
-void BloomFilterDictionary::afegir (string s) {
-    
+BloomFilterDictionary::BloomFilterDictionary () {
+
+    for (int i = 0; i < mida; ++i) {
+        BloomFilter[i] = false;
+    }
 }
 
-bool BloomFilterDictionary::comprovar (string s) {
-    return true;  
+void BloomFilterDictionary::afegir (const string& s) {
+    int a = h1(s);
+    int b = h2(s);
+    int c = h3(s);
+    int d = h4(s);
+    int e = h5(s);
+    int f = h6(s);
+    int g = h7(s);
+
+    BloomFilter[a] = true;
+    BloomFilter[b] = true;
+    BloomFilter[c] = true;
+    BloomFilter[d] = true;
+    BloomFilter[e] = true;
+    BloomFilter[f] = true;    
+    BloomFilter[g] = true;
+
+    cout << s << endl;
+    cout << a << ' ' << b << ' ' << c << endl;
+}
+
+bool BloomFilterDictionary::comprovar (const string& s) {
+    int a = h1(s);
+    int b = h2(s);
+    int c = h3(s);
+    int d = h4(s);
+    int e = h5(s);
+    int f = h6(s);
+    int g = h7(s);    
+
+    return BloomFilter[a] and BloomFilter[b] and BloomFilter[c] and 
+        BloomFilter[d] and BloomFilter[e] and BloomFilter[f] and BloomFilter[g];
+}
+
+void BloomFilterDictionary::imprimir () {
+    for (int i = 0; i < mida; ++i) {
+        cout << BloomFilter[i] << ' ';
+    }
+    cout << endl;
+}
+
+//83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 
+//151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199
+
+int BloomFilterDictionary::h1 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor;
+        hash = hash % mida;
+    }
+
+    return hash;
+}
+
+int BloomFilterDictionary::h2 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%83;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int BloomFilterDictionary::h3 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%89;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int BloomFilterDictionary::h4 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%131;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int BloomFilterDictionary::h5 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%193;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int BloomFilterDictionary::h6 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%197;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int BloomFilterDictionary::h7 (const string& s) {
+    long long int hash = 0;
+
+    for (int i = 0; i < s.size(); ++i) {
+        int valor = (int)s[i];
+        hash = hash + valor%199;
+        hash = hash;// % mida;
+    }
+
+    return hash % mida;
+}
+
+int main () {
+    BloomFilterDictionary b;
+    b.afegir("hola");
+    b.imprimir();
+    b.afegir("bon");
+    b.imprimir();
+    b.afegir("dia");
+    b.imprimir();
+    b.afegir("mec");
+    b.imprimir();
+    //cout << b.comprovar("hola") << ' ' << b.comprovar("adeu");
 }
--- Filtre Bloom/diccBloomFilter.hh
+++ Filtre Bloom/diccBloomFilter.hh
@@ -2,17 +2,45 @@
 #define BLOOM_FILTER
 
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 class BloomFilterDictionary {
     public:
+        //Constructor
         BloomFilterDictionary ();
 
-        void afegir (string s);
+        //Afegir paraula al diccionari
+        void afegir (const string& s);
 
-        bool comprovar(string s);
+        //Comprovar si una paraula és al diccionari
+        bool comprovar(const string& s);
+
+        //Mostra el filtre de Bloom
+        void imprimir ();
     private:
-        
+        //mida del vector bloom
+        int mida = 47925;
+
+        //vector bloom
+        bool BloomFilter [47925];        
+
+        //Funcions de hash
+        int h1 (const string& s);
+
+        int h2 (const string& s);
+
+        int h3 (const string& s);
+
+        int h4 (const string& s);
+
+        int h5 (const string& s);
+
+        int h6 (const string& s);
+
+        int h7 (const string& s);
+
 };
 
 #endif
