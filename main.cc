2eeded2203a931469a53051e17e2877f612533f6
jordipalomeraa <jordipalomera21@gmail.com>
2022-10-13 15:06:33 +0200

    main

--- main.cc
+++ main.cc
@@ -1,10 +1,27 @@
-
-
 #include "./SuperSopa/superSopa.hh"
 
 using namespace std;
 
+void llegir_fitxer (vector<string>& v, const string& path) {
+    ifstream fp_in;
+        string a;
+        fp_in.open(path);
+        while (fp_in >> a) {
+            totes.push_back(a);
+        }
+        fp_in.close(); 
+}
+
 int main () {
+    int n = 10;
+    string path = "./diccionaris/mare-balena-vocabulary-3.txt";
+
+    vector<string> diccionari();
+
+    llegir_fitxer(diccionari, path);
+
+    cout << diccionari[0] << endl;
+
     /*
     // n es la mida de la sopa
     int n;
