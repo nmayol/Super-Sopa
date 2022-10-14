ef206ca80356b450e58a4a2831230db5cfe4268a
pginer22 <paula.munoz.giner@estudiantat.upc.edu>
2022-10-14 21:27:06 +0200

    a

--- Taula Hash/diccDHashing.hh
+++ Taula Hash/diccDHashing.hh
@@ -10,26 +10,23 @@ using namespace std;
 
 class HashTableDictionary {
     public:
-        //constructora
+        //Constructora
         HashTableDictionary (int mida);
-        //Afegir una paraula al diccionari
-        void afegir (string key);
-        //mira si esta la paraula al diccionari
-        bool comprovar(string key);
+        //hashFunctions
         int hash1(string key);
         int hash2(string key);
-        void escriure();
+        //Afegir una paraula al diccionari
+        void afegir (string key);
+        //Mira si la paraula existeix al diccionari
+        bool comprovar(string key);
         
+        //void escriure();
         
     private:
         static const int tableSize;
         int *hashTable;
         int curr_size;
-        int maxcolision;
-    
-
-        
-        
+        int maxcolision;      
 };
 
 #endif
