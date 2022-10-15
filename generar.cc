da7c4db75fee7ff769135f442c8d0a6617e176ca
jordi <jordi.palomera@estudiantat.upc.edu>
2022-10-15 13:31:19 +0200

    guardar sopes al fitxer

--- generar.cc
+++ generar.cc
@@ -1,6 +1,7 @@
 //programa per generar sopes i desar-les en un fitxer
 
 #include <iostream>
+#include <fstream>
 #include "./SuperSopa/superSopa.hh"
 
 using namespace std;
@@ -17,11 +18,28 @@ void llegir_fitxer (vector<string>& v, const string& path) {
 
 //escriu la sopa al final del fitxer path
 void escriure_fitxer(Sopa& sopa, const string& path) {
+    ofstream fw(path, ofstream::out);
 
+    if (fw.is_open()) {
+        int n = sopa.size();
+
+        fw << n << ' ';
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                fw << sopa[i][j] << ' ';
+            }
+        }
+
+        fw << '\n';
+    }
+    else cout << "Problem with opening file";
+
+    fw.close();
 }
 
 int main () {
-     string path = "./diccionaris/mare-balena-vocabulary-3.txt";
+     string path = "./diccionaris/sopes.txt";
 
     vector<string> diccionari;
 
