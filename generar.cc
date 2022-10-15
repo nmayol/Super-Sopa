aeadcebbb41c0a02d00ffea803440693718df273
jordi <jordi.palomera@estudiantat.upc.edu>
2022-10-15 17:25:06 +0200

    generar.cc i experiments filtre de bloom

--- generar.cc
+++ generar.cc
@@ -27,7 +27,7 @@ void escriure_fitxer(Sopa& sopa, const string& path) {
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                fw << sopa[i][j] << ' ';
+                fw << sopa[i][j];
             }
         }
 
@@ -39,20 +39,19 @@ void escriure_fitxer(Sopa& sopa, const string& path) {
 }
 
 int main () {
-     string path = "./diccionaris/sopes.txt";
-
+    string path = "sopes.txt";
     vector<string> diccionari;
-
-    llegir_fitxer(diccionari, path);
-
     superSopa super_sopa;
-    int n = 10; //mida supersopa
 
-    for (int i = 0; i < 100; ++i) {
-        Sopa matriu = Sopa(n, vector<char>(n, '#'));
+    llegir_fitxer(diccionari, path);    
+    
+    for (int n = 10; n <= 55; n += 5) {
+        for (int j = 0; j < 10; ++j) {
+            Sopa matriu = Sopa(n, vector<char>(n, '#'));
 
-        super_sopa.generarSopa(diccionari, matriu);
+            super_sopa.generarSopa(diccionari, matriu);
 
-        escriure_fitxer(matriu, "path del fitxer on guardem les sopes");
-    }
+            escriure_fitxer(matriu, "path del fitxer on guardem les sopes");
+        }
+    }   
 }
