48fbee9c14c936a3b342f20c5d3c387635ad6f40
jordi <jordi.palomera@estudiantat.upc.edu>
2022-10-16 12:21:09 +0200

    a

--- experiment_hash.cc
+++ experiment_hash.cc
@@ -21,7 +21,7 @@ auto moment () {
     return chrono::steady_clock::now();
 }
 
-void afegir_prefix(HashTableDictionary& d, string s) {
+void afegir_prefix(TrieDictionary& d, string s) {
     int n = s.size();
 
     string aux = "";
@@ -63,8 +63,8 @@ int main () {
 
     llegir_fitxer(diccionari, pathDiccionari);
 
-    HashTableDictionary hash_table(diccionari.size());
-    HashTableDictionary prefixos(diccionari.size()*10);  
+    TrieDictionary hash_table;
+    TrieDictionary prefixos;  
 
     for (int i = 0; i < diccionari.size(); ++i) {
         afegir_prefix(prefixos, diccionari[i]);
